36cde4af6d9529aa10ee5a437b8d54d9ca8982bb
qq <441627093@qq.com>
2024-07-01 17:51:18 +0800

    后端

---
+++ acm/src/main/java/com/example/acm/AcmApplication.java
@@ -0,0 +1,13 @@
+package com.example.acm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmApplication.class, args);
+    }
+
+}
---
+++ acm/src/test/java/com/example/acm/AcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
