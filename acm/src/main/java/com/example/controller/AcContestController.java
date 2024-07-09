ba9212b8f3cdc0a1836d3593e2c829d9caa492e5
qq <441627093@qq.com>
2024-07-09 10:59:14 +0800

    使用knife4j写接口文档

--- acm/src/main/java/com/example/controller/AcContestController.java
+++ acm/src/main/java/com/example/controller/AcContestController.java
@@ -8,26 +8,23 @@ import com.example.common.QueryPageParam;
 import com.example.common.Result;
 import com.example.entity.AcContest;
 import com.example.service.AcContestService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/acContest")
+@Api(tags = "AtCoder比赛列表")
 public class AcContestController {
     @Autowired
     private AcContestService accontestService;
 
-    // 查询全部
-    @GetMapping("/list")
-    public List<AcContest> list() {
-        return accontestService.list();
-    }
-
 
     // 根据比赛名称模糊查询
     @PostMapping("/listP")
+    @ApiOperation("根据比赛名称模糊查询")
     public Result listP(@RequestBody AcContest accontest) {
         LambdaQueryWrapper<AcContest> queryWrapper = new LambdaQueryWrapper<>();
         if (accontest.getAcContest() != null) {
@@ -38,6 +35,7 @@ public class AcContestController {
 
     // 分页查询
     @PostMapping("/listPage")
+    @ApiOperation("分页查询")
     public Result listPage(@RequestBody QueryPageParam query) {
         String name = (String) query.getParam().get("acContest");
 
--- acm/src/main/java/com/example/controller/StudentController.java
+++ acm/src/main/java/com/example/controller/StudentController.java
@@ -8,6 +8,8 @@ import com.example.common.QueryPageParam;
 import com.example.common.Result;
 import com.example.entity.Student;
 import com.example.service.StudentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +17,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
+@Api(tags = "学生用户管理")
 public class StudentController {
     @Autowired
     private StudentService studentService;
 
     //用户登录
     @PostMapping("/login")
+    @ApiOperation("用户登录")
     public Result login(@RequestBody Student student){
         List list= studentService.lambdaQuery()
                 .eq(Student::getStuUsername,student.getStuUsername())
@@ -30,36 +34,35 @@ public class StudentController {
     }
     //用户注册
     @PostMapping("/register")
+    @ApiOperation("用户注册")
     public Result register(@RequestBody Student student){
         return studentService.save(student)?Result.suc():Result.fail();
     }
 
-    //查询全部
-    @GetMapping("/list")
-    public List<Student> list(){
-        return studentService.list();
-    }
-
     //新增
     @PostMapping
+    @ApiOperation("新增")
     public Result save(@RequestBody Student student){
         return studentService.save(student)?Result.suc():Result.fail();
     }
 
     //修改
     @PutMapping
+    @ApiOperation("修改")
     public Result mod(@RequestBody Student student){
         return studentService.updateById(student)?Result.suc():Result.fail();
     }
 
     //删除
     @DeleteMapping("/{studentNo}")
+    @ApiOperation("删除")
     public Result delete(@PathVariable String studentNo){
         return studentService.removeById(studentNo)?Result.suc():Result.fail();
     }
 
     //模糊查询
     @PostMapping("/listP")
+    @ApiOperation("根据学号模糊查询")
     public Result listP(@RequestBody Student student){
         LambdaQueryWrapper<Student> lambdaQueryWrapper = new LambdaQueryWrapper<>();
         if(student.getStuNum()!=null)
@@ -69,6 +72,7 @@ public class StudentController {
 
     //分页查询
     @PostMapping("/listPage")
+    @ApiOperation("分页查询")
     public Result listPage(@RequestBody QueryPageParam query){
         String name= (String) query.getParam().get("stuNum");
 
